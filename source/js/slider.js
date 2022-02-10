fe27de54aaddef68f26f6805bfcd00adfd6ebd48
olga2102 <70343745+olga2102@users.noreply.github.com>
2022-02-10 21:11:21 +0300

    portfolio desktop

---
+++ source/js/slider.js
@@ -0,0 +1,33 @@
+// import Swiper, { Navigation, Pagination } from 'swiper';
+
+// import 'swiper/css';
+// import 'swiper/css/navigation';
+// import 'swiper/css/pagination';
+
+new Swiper(".portfolio__swiper", {
+  navigation: {
+    nextEl: '.swiper-button--next',
+    prevEl: '.swiper-button--prev',
+  },
+  pagination: {
+    el: '.swiper-pag',
+    clickable: true,
+  },
+  breakpoints: {
+    360: {
+    slidesPerView: 1,
+    },
+    770: {
+      slidesPerView: 2,
+      },
+    // 978: {
+    //   slidesPerView: 3,
+    // },
+    1024: {
+      slidesPerView: 4,
+    }
+  },
+  spaceBetween: 35,
+  loop: true,
+});
+
